e9b1be72352f1200156f5aa03411739ffdeeb641
Edenilton <142240436+Edenilton2023@users.noreply.github.com>
2023-10-13 12:05:39 -0300

    menu flex

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+function show() {
+   let menu = document.querySelector(".abalt");
+   if (menu.style.display === 'none' || menu.style.display === '') {
+       menu.style.display = 'flex';
+   } else {
+       menu.style.display = 'none';
+   }
+}
+
+
+
+
+
+
+
+
